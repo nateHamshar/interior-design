8d8113121ee4b27a742afe8d61c626b3ed750d61
Nate Hamshar <williamhamshar@gmail.com>
2022-06-17 09:51:42 -0400

    adding photos to files

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="landing-page">
+                <h1 className="title">Design Hub</h1>
+                <p className="landing-message">Your house should fit you</p>
+            </div>
+        </div>
+    );
+}
+ 
+export default Home;
